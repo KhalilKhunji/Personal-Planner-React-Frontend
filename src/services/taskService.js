0e58b3a5758937671cb30a5a51edd634b2f3b8f5
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-08-18 16:34:19 +0300

    deleted draft service file

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,3 +1,17 @@
-const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/tasks/`;
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/tasks`;
 
-export {};
+const index = async () => {
+  try {
+    const response = await fetch(BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+
+export default { index };
