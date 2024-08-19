6244bcd83cce9ca1007a0fbadc3a83e8edd9f102
YUSUF <Yousifcool889@gmail.com>
2024-08-19 16:17:53 +0300

    pull issue fixed

--- src/components/TaskDetails/TaskDetails.jsx
+++ src/components/TaskDetails/TaskDetails.jsx
@@ -26,27 +26,41 @@ const TaskDetails = ({ user }) => {
         getTask();
     }, [taskId]);
 
-    if (!task) return <main>Loading Task...</main>
+  const handleAddItem = async (todolist) => {
+    const newItem = await itemService.create(taskId, todolist);
+    const copyItem = { ...task };
+    copyItem.items.push(newItem);
 
-    return(
-        <>
-            <h1>{task.name}</h1>
-            <h2>{user.username} TodoLists: </h2>
-            {task.items.map((item) => (
-                <div key={item._id}>
-                    <input type="checkbox" name="list-items"/>
-                    <label htmlFor="list-items">{item.text}</label>
-                </div>
-            ))}
-            <h2>{user.username} Notes: </h2>
-            {task.notes.map((note) => (
-                <div key={note._id}>
-                    <h3>Title: {note.title}</h3>
-                    <p>Content: {note.content}</p>
-                </div>
-            ))} 
-        </>
-    )
+    setTask(copyItem);
+  };
+
+  if (!task) return <main>Loading Task...</main>;
+
+  return (
+    <>
+      <h1>{task.name}</h1>
+      <h2>{user.username} TodoLists: </h2>
+      {task.items.map((item) => (
+        <div key={item._id}>
+          <input type="checkbox" name="list-items" />
+          <label htmlFor="list-items">{item.text}</label>
+        </div>
+      ))}
+      <br />
+      {showForm ? (
+        <ItemForm handleAddItem={handleAddItem} />
+      ) : (
+        <button onClick={() => setShowForm(true)}>Add Item</button>
+      )}
+      <h2>{user.username} Notes: </h2>
+      {task.notes.map((note) => (
+        <div key={note._id}>
+          <h3>Title: {note.title}</h3>
+          <p>Content: {note.content}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default TaskDetails;
